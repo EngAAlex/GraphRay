d8c4151dca9b90a9733d7b64d0ca16cccc4e605b
Alessio Arleo <ingarleo@icloud.com>
2017-08-11 01:00:43 -0700

    Added license information

--- graphray-utils/src/main/java/com/graphray/utils/Toolbox.java
+++ graphray-utils/src/main/java/com/graphray/utils/Toolbox.java
@@ -1,6 +1,18 @@
-/**
+/*******************************************************************************
+ * Copyright 2017 Alessio Arleo
  * 
- */
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
 package com.graphray.utils;
 
 import java.io.IOException;
@@ -21,10 +33,6 @@ import com.graphray.common.vertextypes.PathfinderVertexID;
 import com.graphray.common.vertextypes.PathfinderVertexType;
 import com.graphray.common.writables.SetWritable;
 
-/**
- * @author spark
- *
- */
 public class Toolbox {
 	
 //	protected static Logger log = Logger.getLogger(Toolbox.class);
